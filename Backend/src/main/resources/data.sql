9acdec2fe592ca0cfc02f630437a2f95fd0b9e76
hrcak <mali.patuljko@gmail.com>
2019-10-17 23:10:12 +0200

    changed data and added Cors config

--- Backend/src/main/resources/data.sql
+++ Backend/src/main/resources/data.sql
@@ -1,22 +1,22 @@
 insert into territory (id, city, postal, country)
-values (1, 'Belgrade', '11000', 'Serbia');
+values (10000, 'Belgrade', '11000', 'Serbia');
 insert into territory (id, city, postal, country)
-values (2, 'Zurich', '8000', 'Switzerland');
+values (20000, 'Zurich', '8000', 'Switzerland');
 insert into territory (id, city, postal, country)
-values (3, 'Budapest', '1007', 'Hungary');
+values (30000, 'Budapest', '1007', 'Hungary');
 
 insert into shop (id, name, address, phone, territory_id)
-values(1, 'Gatto', 'Ruzveltova 4', '+3816520585', 1);
+values(1, 'Gatto', 'Ruzveltova 4', '+3816520585', 10000);
 insert into shop (id, name, address, phone, territory_id)
-values(2, 'ITKitty', 'Pozarevacka 16', '+381661155285', 1);
+values(2, 'ITKitty', 'Pozarevacka 16', '+381661155285', 10000);
 insert into shop (id, name, address, phone, territory_id)
-values(3, 'Dataquest', 'Weinbergstrasse 71', '+414436015648', 2);
+values(3, 'Dataquest', 'Weinbergstrasse 71', '+414436015648', 20000);
 insert into shop (id, name, address, phone, territory_id)
-values(4, 'PremiumIT', 'Lagerhausstrasse 18', '+4152361606711', 2);
+values(4, 'PremiumIT', 'Lagerhausstrasse 18', '+4152361606711', 20000);
 insert into shop (id, name, address, phone, territory_id)
-values(5, 'PCLand', 'Jasz u. 71', '+36568002585', 3);
+values(5, 'PCLand', 'Jasz u. 71', '+36568002585', 30000);
 insert into shop (id, name, address, phone, territory_id)
-values(6, 'MagiComp', 'Bajcsy-Zsilinszky u. 60', '+361828205463', 3);
+values(6, 'MagiComp', 'Bajcsy-Zsilinszky u. 60', '+361828205463', 30000);
 
 insert into employee (id, firstName, lastName, age, position, shop_id)
 values(1, 'Jovana', 'Ilic', 29, 'register', 1);
@@ -31,12 +31,12 @@ values(5, 'Adrienn', 'Tot', 33, 'advertising', 5);
 insert into employee (id, firstName, lastName, age, position, shop_id)
 values(6, 'Marko', 'Horvat', 29, 'tech support', 6);
 
-insert into territory_shops (territory_id, shops_id) values (1,1);
-insert into territory_shops (territory_id, shops_id) values (1,2);
-insert into territory_shops (territory_id, shops_id) values (2,3);
-insert into territory_shops (territory_id, shops_id) values (2,4);
-insert into territory_shops (territory_id, shops_id) values (3,5);
-insert into territory_shops (territory_id, shops_id) values (3,6);
+insert into territory_shops (territory_id, shops_id) values (10000,1);
+insert into territory_shops (territory_id, shops_id) values (10000,2);
+insert into territory_shops (territory_id, shops_id) values (20000,3);
+insert into territory_shops (territory_id, shops_id) values (20000,4);
+insert into territory_shops (territory_id, shops_id) values (30000,5);
+insert into territory_shops (territory_id, shops_id) values (30000,6);
 
 insert into shop_employees (shop_id, employees_id) values (1,1);
 insert into shop_employees (shop_id, employees_id) values (2,2);
